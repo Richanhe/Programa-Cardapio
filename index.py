a57eb843b12e81d9316c5ffb6c17237de2447e91
Richard Oliveira <richarok18@gmail.com>
2024-05-28 22:15:11 -0300

    start
    start of the project

--- index.py
+++ index.py
@@ -1,1 +1,26 @@
-jjjj
+cardapio = {}
+
+while True:
+    entrada = input()
+
+    if entrada == "add":
+        categoria = input("Qual a categoria do item? ")
+        item = input("Qual o item? ")
+        produto = input("Qual o produto? ")
+        preço = input("Qual o preço? ")
+        if len(cardapio) == 0:
+            cardapio[categoria] = {}
+        if len(cardapio[categoria]) == 0:
+            cardapio[categoria][item] = {}
+        cardapio[categoria][item][produto] = preço
+    #elif separada[0] == "rmv":
+
+    #elif separada[0] == "edt":
+
+    #elif separada[0] == "src":
+
+    elif entrada == "all":
+        print(cardapio)
+
+    elif entrada == "stop":
+        break 
